71c38ad9f32bcb001b79ee616cdb4861812f87be
Pedro Lucas <pedrolucaspbi@gmail.com>
2022-01-10 19:53:21 -0300

    Sequência de Fibonacci

---
+++ Projeto/Sequenciadefibonacci.py
@@ -0,0 +1,13 @@
+print('----------------- \033[1;35mSEQUÊNCIA DE FIBONACCI\033[m -----------------')
+n = int(input('Quantos termos voce quer mostrar?: '))
+t1 = 0
+t2 = 1
+cont = 3
+print('{} {}'.format(t1, t2), end='')
+while cont <= n:
+    t3 = t1 + t2
+    print(' {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' FIM')
